3f4c7666d21cdf482473cd49f09e4d1a06f2399e
Ryan Hadfield <ryanhadfield@gmail.com>
2021-05-04 11:54:16 -0600

    Create seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
